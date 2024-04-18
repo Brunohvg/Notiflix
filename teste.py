85135a0d921a5ae77d0c418883c9c5dbd0415681
Bruno Vidal <brunovidal27.19@gmail.com>
2024-04-18 17:01:45 -0300

    arquivos

--- teste.py
+++ teste.py
@@ -1,1 +1,13 @@
 # ominio=ngrok http --domain=goblin-romantic-imp.ngrok-free.app 8000
+"""SECRET_KEY=django-insecure-lq5q1gnzs(_b1&q&t501p5!)$+pn9!o@mkj-agiu3$65@%8vg4
+DEBUG=True
+
+ALLOWED_HOSTS=localhost,127.0.0.1,1b8c-2804-1b3-61c0-f1ab-d556-9931-a8a1-5a74.ngrok.io
+
+#Dados Aplicativo Nuvemshop
+
+CLIENT_ID=8619
+CLIENT_SECRET=4263cfda2bc47a702f62d74784d7abcd97a237e1e5c3875b
+
+
+dominio=ngrok http --domain=goblin-romantic-imp.ngrok-free.app 8000"""
