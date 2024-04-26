6625b6bafdea1461a147cbd7b11f4b14d1572f3b
Bruno Vidal <brunovidal27.19@gmail.com>
2024-04-25 23:18:40 -0300

    tete

--- app_profile/models.py
+++ app_profile/models.py
@@ -6,6 +6,12 @@ class Profile(models.Model):
     nome = models.CharField(max_length=125)
     whatsapp = models.CharField(max_length=15)
     user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
+    # email financeiro
+    # cep
+    # Endereço
+    # cidade
+    # estado
+    # documento
 
     def __str__(self) -> str:
 
