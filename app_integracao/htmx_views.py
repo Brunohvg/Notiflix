b7e2fcaaaf4e9e28663c30c3f9313e5ee7b43be1
Bruno Vidal <brunovidal27.19@gmail.com>
2024-05-22 17:33:11 -0300

    mudancas

--- app_integracao/htmx_views.py
+++ app_integracao/htmx_views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 def check_qrcode(request):
     qrcode = request.GET.get("qrcode")
     return HttpResponse(f"qrcode:{qrcode} ")
+
+
+def exibir_qrcode(request): ...
