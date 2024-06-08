6114063d979ab0cfda1065e7c8f572bf5592d457
Bruno Vidal <brunovidal27.19@gmail.com>
2024-06-08 13:39:51 -0300

    1

--- app_pedido/signals.py
+++ app_pedido/signals.py
@@ -7,7 +7,6 @@ from app_mensagem.models import MensagemPadrao
 WHATSAPP = Whatsapp()
 
 
-# TODO Conferir status para verificação
 @receiver(post_save, sender=Pedido)
 def _enviar_msg(sender, instance, created, **kwargs):
     phone = instance.cliente.contact_phone
@@ -15,48 +14,46 @@ def _enviar_msg(sender, instance, created, **kwargs):
     status = instance.status_envio
     loja = instance.loja.nome
     user = instance.loja.usuario
+    rastreio = instance.codigo_rastreio
+    instance_name = instance.loja.whatsapp.instanceId
+    apikey = instance.loja.whatsapp.token
+    print(instance_name)
+    tipo_mensagem = {
+        "processando": "Pedido Pago",
+        "embalado": "Pedido Embalado",
+        "enviado": "Pedido Enviado",
+        "cancelado": "Pedido Cancelado",
+    }
 
-    if created:
-        print(f"Se ha creado una nueva instancia: {phone}")
+    tipo_pedido = tipo_mensagem.get(status.lower())
+
+    if tipo_pedido:
+        try:
+            # Obtém todas as mensagens padrão para o tipo de pedido e usuário específico
+            msgs = MensagemPadrao.objects.filter(tipo_pedido=tipo_pedido, usuario=user)
+            for msg in msgs:
+                # Substitui as variáveis nas mensagens padrão
+                texto_formatado = msg.mensagem_padrao.replace("[nome_cliente]", nome)
+                texto_formatado = texto_formatado.replace(
+                    "[numero_pedido]", str(instance.id_pedido)
+                )
+                texto_formatado = texto_formatado.replace("[nome_loja]", loja)
+                texto_formatado = texto_formatado.replace("[link_rastreio]", rastreio)
+                # Envia cada mensagem via WhatsApp
+                if msg.ativado:
+                    WHATSAPP._enviar_msg(
+                        instance_name=instance_name,
+                        apikey=apikey,
+                        number_phone=f"55{phone}",
+                        texto=texto_formatado,
+                    )
+                    print(f"Mensagem enviada para {phone}: {texto_formatado}")
+                else:
+                    print("Mensagem não pode ser enviada")
+        except MensagemPadrao.DoesNotExist:
+            print(
+                f"Nenhuma mensagem padrão encontrada para o tipo de pedido '{tipo_pedido}' e usuário {user}"
+            )
     else:
-        tipos_pedidos = {
-            "pago": "Pedido Pago",
-            "embalado": "Pedido Embalado",
-            "enviado": "Pedido Enviado",
-            "cancelado": "Pedido Cancelado",
-        }
-
-        tipo_pedido = tipos_pedidos.get(status.lower())
-
-        if tipo_pedido:
-            try:
-                # Obtém todas as mensagens padrão para o tipo de pedido e usuário específico
-                msgs = MensagemPadrao.objects.filter(
-                    tipo_pedido=tipo_pedido, usuario=user
-                )
-                for msg in msgs:
-                    # Substitui as variáveis nas mensagens padrão
-                    texto_formatado = msg.mensagem_padrao.replace(
-                        "[nome_cliente]", nome
-                    )
-                    texto_formatado = texto_formatado.replace(
-                        "[numero_pedido]", str(instance.id_pedido)
-                    )
-                    texto_formatado = texto_formatado.replace("[nome_loja]", loja)
-                    # Envia cada mensagem via WhatsApp
-                    if msg.ativado:
-                        WHATSAPP._enviar_msg(
-                            instance_name="319924305000",
-                            apikey="7fbhgrqj4d2x5vf0betit",
-                            number_phone=f"55{phone}",
-                            texto=texto_formatado,
-                        )
-                        print(f"Mensagem enviada para {phone}: {texto_formatado}")
-                    print("nao pode ser enviada")
-            except MensagemPadrao.DoesNotExist:
-                print(
-                    f"Nenhuma mensagem padrão encontrada para o tipo de pedido '{tipo_pedido}' e usuário {user}"
-                )
-        else:
-            print(f"Status do pagamento não é reconhecido: {status}")
-        print(f"Se ha actualizado la instancia: {phone}")
+        print(f"Status do pagamento não é reconhecido: {status}")
+    print(f"Se ha actualizado la instancia: {phone}")
