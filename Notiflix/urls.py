9e474ed08effc0f8798fcfed10f32bfea2f953b8
Bruno Vidal <brunovidal27.19@gmail.com>
2024-07-22 17:46:47 -0300

    Urls arquivos static

--- Notiflix/urls.py
+++ Notiflix/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -11,3 +12,8 @@ urlpatterns = [
     path("", include("app_webhook.urls")),
     path("", include("app_mensagem.urls")),
 ]
+
+# Add static and media URL patterns only if in DEBUG mode
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
