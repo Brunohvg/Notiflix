fe421250d9f4b0c597e460f7b1ae1c969af32835
Bruno Vidal <brunovidal27.19@gmail.com>
2024-08-25 01:06:13 -0300

    modificações

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,8 @@
 # Executar migrações
 python manage.py migrate
 
+# Coletar arquivos estáticos
+python manage.py collectstatic --noinput
+
 # Iniciar o Gunicorn
 exec gunicorn Notiflix.wsgi:application --bind 0.0.0.0:8000
