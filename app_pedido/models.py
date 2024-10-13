99a86d0f58ef9d160d25455210829dcc025fc08a
Bruno Vidal <brunovidal27.19@gmail.com>
2024-10-12 23:48:48 -0300

    token

--- app_pedido/models.py
+++ app_pedido/models.py
@@ -36,7 +36,7 @@ class Pedido(models.Model):
     data_reembolsado = models.DateField(verbose_name="Data Reembolsado", null=True, blank=True)
     codigo_rastreio = models.CharField(max_length=100, verbose_name="Código de Rastreio", blank=True)
     ultimo_status_notificado = models.CharField(max_length=100, verbose_name="Último Status Notificado", blank=True, null=True)
-
+    token_pedido = models.CharField(max_length=100, verbose_name="Token Pedido", blank=True, null=True)
     def __str__(self):
         return f"Pedido {self.id_venda} - Cliente: {self.cliente.contact_name}"
 
