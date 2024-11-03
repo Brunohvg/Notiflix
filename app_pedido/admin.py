b9af69626b00b7365e81ca873bde70709f60d7dd
Bruno Vidal <brunovidal27.19@gmail.com>
2024-11-03 09:36:12 -0300

    registro app_endereço

--- app_pedido/admin.py
+++ app_pedido/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Cliente, Pedido
+from .models import Cliente, Pedido, Endereco
 # Register your models here.
 admin.site.register(Cliente)
 admin.site.register(Pedido)
+admin.site.register(Endereco)
